4d28192b6a314b8c2c2bd9c96b598f18b149d000
Jang <ahhhaaah@gmail.com>
2023-01-12 03:12:04 +0900

    Initial commit

---
+++ src/main/java/com/project/MainController.java
@@ -0,0 +1,18 @@
+package com.project;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@Log4j
+public class MainController {
+
+    @RequestMapping(value = "/")
+    public ModelAndView index(ModelAndView mv){
+        log.info("MainPage");
+        mv.setViewName("index");
+        return mv;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%@page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Matdongsan</title>
+</head>
+<body>
+
+
+<%--<jsp:include page="common/header.jsp" />--%>
+
+
+
+
+</body>
+</html>
