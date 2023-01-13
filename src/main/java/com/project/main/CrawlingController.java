a046086a27a2251134a3de8cde0926be29327f0a
JinHoYY <yangkang9879@naver.com>
2023-01-13 21:48:04 +0900

    feat : 크롤링 작업중(주석처리)

--- src/main/java/com/project/main/CrawlingController.java
+++ src/main/java/com/project/main/CrawlingController.java
@@ -1,9 +1,11 @@
 /*
+
 package com.project.main;
 
 import org.springframework.ui.Model;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
@@ -12,14 +14,28 @@ public class CrawlingController {
     public static HashMap<String, String> map;
 
     public String startCrawl(Model model) {
-        SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd", Locale.KOREA);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd", Locale.KOREA);
         Date currentTime = new Date();
 
-        String dTime = format.format(currentTime);
+        String dTime = formatter.format(currentTime);
         String e_date = dTime;
 
         currentTime.setDate(currentTime.getDate() -1);
+        String s_date = formatter.format(currentTime);
+
+        String query = "";
+        String s_from = s_date.replace(".", "");
+        String e_to = e_date.replace(".", "");
+
+        int page = 1;
+        ArrayList<String> list1 = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+
+        while (page < 20){
+            String address =
+        }
 
     }
 }
+
 */
