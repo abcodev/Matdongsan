d000209593ae3c5231df1479b40c8ab47967904c
yisoo98776 <116542266+yisoo98776@users.noreply.github.com>
2023-01-13 21:13:21 +0900

    board 파일 추가

--- src/main/java/com/project/board/freeBoard/service/freeBoardService.java
+++ src/main/java/com/project/board/freeBoard/service/freeBoardService.java
@@ -1,4 +1,7 @@
 package com.project.board.freeBoard.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class freeBoardService {
 }
--- src/main/java/com/project/board/qnaBoard/vo/QnaBoard.java
+++ src/main/java/com/project/board/qnaBoard/vo/QnaBoard.java
@@ -1,4 +1,19 @@
 package com.project.board.qnaBoard.vo;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class QnaBoard {
+    private int qnaBno;
+    private int memberNo;
+    private int pBno;
+    private String qnaTitle;
+    private String qnaContent;
+    private Date qnaDate;
+    private int count;
+    private String status;
+    private String qnaArea;
+
 }
