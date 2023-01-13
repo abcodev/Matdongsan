a7d0aea953af84ca9f437a4bd7fbe43d62d271ce
JinHoYY <yangkang9879@naver.com>
2023-01-13 21:23:01 +0900

    feat : 크롤링 작업중(주석처리)

---
+++ src/main/java/com/project/main/CrawlingController.java
@@ -0,0 +1,25 @@
+/*
+package com.project.main;
+
+import org.springframework.ui.Model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+
+public class CrawlingController {
+    public static HashMap<String, String> map;
+
+    public String startCrawl(Model model) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy.MM.dd", Locale.KOREA);
+        Date currentTime = new Date();
+
+        String dTime = format.format(currentTime);
+        String e_date = dTime;
+
+        currentTime.setDate(currentTime.getDate() -1);
+
+    }
+}
+*/
