34d9c7296b25b78537e6f286b7e6f81046cb0cb2
JinHoYY <yangkang9879@naver.com>
2023-01-17 20:43:50 +0900

    feat : 크롤링 작업중 (한개 띄움)

--- src/main/java/com/project/MainController.java
+++ src/main/java/com/project/MainController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
@@ -30,26 +32,41 @@ public class MainController {
 
     @RequestMapping(value = "/mainPage")
     public String getStockPrice(Model model) throws IOException {
+
         String url = "https://land.naver.com/news/region.naver?city_no=1100000000&dvsn_no=1168000000";
 
         Document doc = Jsoup.connect(url).get();
 
         Elements e1 = doc.getElementsByAttributeValue("class", "section_headline");
         Element e2 = e1.get(0);
-        Elements e3 = e2.select("dd");
-
-        String news = e3.get(0).text();
-        model.addAttribute("news", news);
 
         Elements photoElements = e2.getElementsByAttributeValue("class", "photo");
-        Element articleElement = photoElements.get(0);
-        Elements aElements = articleElement.select("a");
-        Element aElement = aElements.get(0);
-        Element imgElement = aElement.select("img").get(0);
-//        String imgUrl = imgElement.attr("src"); // 사진링크
-        String title = imgElement.attr("alt"); //기사제목
 
-        model.addAttribute("title", title);
+        ArrayList<String> newsTitle = null;
+        ArrayList<String> newsUrl= null;
+
+        for(int i = 0; i< 4; i++){
+            Element articleElement = photoElements.get(i);
+            Elements aElements = articleElement.select("a");
+            Element aElement = aElements.get(0);
+
+            String articleUrl = "https://land.naver.com"+aElement.attr("href"); // 기사링크
+            Element link = doc.select("a").first();
+
+
+            Element imgElement = aElement.select("img").get(0);
+//            String imgUrl = imgElement.attr("src"); // 사진링크
+            String title = imgElement.attr("alt"); //기사제목
+
+            System.out.println(title);
+            System.out.println(articleUrl);
+
+            newsTitle.add(title);
+            newsUrl.add(articleUrl);
+        }
+
+        model.addAttribute("newsTitle", newsTitle.get(0));
+        model.addAttribute("newsUrl", newsUrl.get(0));
 
         return "common/mainPage";
     }
