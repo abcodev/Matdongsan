a94670388d3863e7d26989dc546ddc9cfd179f0c
kayoung0814 <116356647+kayoung0814@users.noreply.github.com>
2023-01-19 21:14:37 +0900

    feat:ajax 작업중. mapping안되는 오류

--- src/main/java/com/project/realestate/controller/RealEstateController.java
+++ src/main/java/com/project/realestate/controller/RealEstateController.java
@@ -25,6 +25,37 @@ public class RealEstateController {
         this.realEstateService = realEstateService;
     }
 
+    @RequestMapping("/list")
+    public String realEstatePage(Model model, @RequestParam(value="currentPage",defaultValue="1")int currentPage,
+                                 @RequestParam Map<String, Object> paramMap) {
+        Map<String, Object> map = new HashMap();
+        List<RealEstateRent> localList = new ArrayList<>();
+
+        //select dong 옵션 받아오기
+        List<String> optionList = new ArrayList();
+
+        if (paramMap.get("selectOption1") == null) { // 검색 요청을 하지 않은 경우
+
+            map = realEstateService.selectList(currentPage);
+            localList = realEstateService.searchLocalList();
+
+        } else { // 검색 요청을 한 경우
+            // 검색에 필요한 데이터를 paramMap을 넣어서 호출
+            // condition, keyword
+            paramMap.put("currentPage", currentPage);
+
+            // 2. 게시글 셀렉트
+            map = realEstateService.selectList(paramMap);
+        }
+
+        //3. 페이징 포워딩(pi 객체와 list 객체 저장시키면서)
+        model.addAttribute("map", map);
+        model.addAttribute("localList", localList);
+        model.addAttribute("optionList", optionList);
+
+        return "realestate/realestateList";
+    }
+
     @RequestMapping("/list/{option1}")
     public String realEstatePage(Model model, @RequestParam(value="currentPage",defaultValue="1")int currentPage,
                                  @RequestParam Map<String, Object> paramMap,
