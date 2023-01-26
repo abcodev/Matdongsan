b13a88eeafaa8d9d276bbfd09bfc045a94dbeef7
JinHoYY <yangkang9879@naver.com>
2023-01-26 20:04:35 +0900

    feat : 지도 작업중

--- src/main/java/com/project/realestate/dto/RealEstateMainListDto.java
+++ src/main/java/com/project/realestate/dto/RealEstateMainListDto.java
@@ -9,10 +9,8 @@ public class RealEstateMainListDto {
 
     private String address;
     private String objAmt;
+
     private String bldgNm;
-//    private String sggNm;
-//    private String bjdongNm;
-//    private String bonbeon;
-//    private String bubeon;
+
 
 }
