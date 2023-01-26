dc8e70ea5e04ea6ca55a36d8fa984353f99fa109
yeomseohak <duatjgkr123@gmail.com>
2023-01-27 01:46:08 +0900

    feat : client sockJs

--- src/main/java/com/project/chat/config/WebSocketConfig.java
+++ src/main/java/com/project/chat/config/WebSocketConfig.java
@@ -12,8 +12,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chatting") // 연결될 엔드 포인트
-//                .setAllowedOrigins("*")
+        registry.addEndpoint("/mainPage") // 연결될 엔드 포인트
+                .setAllowedOrigins("http://localhost:8070/Matdongsan")
                 .withSockJS(); // SocketJS 를 연결한다는 설정
     }
     @Override
