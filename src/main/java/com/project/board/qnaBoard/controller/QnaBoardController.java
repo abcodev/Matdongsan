77e5d76aa106aea329f2750130be7a215992f8f1
kayoung0814 <116356647+kayoung0814@users.noreply.github.com>
2023-01-27 17:07:20 +0900

    feat: 자유게시판 등록중

--- src/main/java/com/project/board/qnaBoard/controller/QnaBoardController.java
+++ src/main/java/com/project/board/qnaBoard/controller/QnaBoardController.java
@@ -1,7 +1,5 @@
 package com.project.board.qnaBoard.controller;
 
-import com.project.board.freeBoard.service.FreeBoardService;
-import com.project.board.freeBoard.vo.FreeBoard;
 import com.project.board.qnaBoard.service.QnaBoardService;
 import com.project.board.qnaBoard.service.QnaBoardServiceImpl;
 import com.project.board.qnaBoard.vo.BoardType;
@@ -34,9 +32,6 @@ public class QnaBoardController {
 
     @Autowired
     private QnaBoardService boardService;
-
-//    @Autowired
-//    private FreeBoardService freeBoardService;
 
 
     @RequestMapping("/list/{boardCode}")
@@ -80,17 +75,14 @@ public class QnaBoardController {
     @RequestMapping("/insert/{boardCode}")
     public String insertQboard(
             @PathVariable("boardCode") String boardCode,
-            Model model, QnaBoard qb, FreeBoard freeBoard, HttpSession session){
-
-        int result = 0;
+            Model model, QnaBoard qb,HttpSession session){
 
 
-            result = boardService.insertQboard(qb);
+        int result = boardService.insertQboard(qb);
 
 
         return "redirect:/board/list/C";
-
-    }
+        }
 
 
     /*답글달기 페이지*/
