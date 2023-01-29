08fc2aafe15afc8724175d0030d94f0abd34bdff
Jang <ahhhaaah@gmail.com>
2023-01-29 20:21:42 +0900

    feat: 병합

--- src/main/java/com/project/board/dao/FreeBoardDao.java
+++ src/main/java/com/project/board/dao/FreeBoardDao.java
@@ -16,35 +16,8 @@ import java.util.Map;
 @Repository
 public class FreeBoardDao {
 
-
-    public int selectFlistCount(SqlSession sqlSession) {
-        return sqlSession.selectOne("boardMapper.selectFlistCount");
-    }
-
-    public int selectFlistCount(SqlSession sqlSession , Map<String, Object> paramMap) {
-        return sqlSession.selectOne("boardMapper.searchFlistCount", paramMap);
-    }
-
-    public ArrayList<QnaBoard> selectFlist(SqlSession sqlSession, PageInfo pi ){
-        int offset = (pi.getCurrentPage() -1) * pi.getBoardLimit();
-        int limit = pi.getBoardLimit();
-
-        RowBounds rowBounds = new RowBounds(offset,limit);
-
-        return (ArrayList) sqlSession.selectList("boardMapper.selectFlist",null,rowBounds);
-    }
-    public ArrayList<QnaBoard> selectFlist(SqlSession sqlSession, Map<String,Object> paramMap){
-        int offset = ( ((PageInfo)paramMap.get("pi")).getCurrentPage() -1) * ((PageInfo)paramMap.get("pi")).getBoardLimit();
-        int limit = ((PageInfo)paramMap.get("pi")).getBoardLimit();
-
-        RowBounds rowBounds = new RowBounds(offset,limit);
-
-        return (ArrayList) sqlSession.selectList("boardMapper.searchFlist", paramMap ,rowBounds);
-    }
-
-
     public int insertFboard(SqlSession sqlSession, FreeBoard fb){
-        return sqlSession.insert("boardMapper.insertFboard", fb);
+        return sqlSession.insert("freeBoardMapper.insertFboard", fb);
     }
 
     public List<FreeBoard> selectFreeList(SqlSession sqlSession) {
