2826c2874d3ec8bf2037b9180f1b2573e4a059e6
yeomseohak <duatjgkr123@gmail.com>
2023-01-29 19:43:11 +0900

    feat : FreeBoard start

--- src/main/java/com/project/board/controller/FreeBoardContoller.java
+++ src/main/java/com/project/board/controller/FreeBoardContoller.java
@@ -1,26 +1,33 @@
 package com.project.board.controller;
 
 import com.project.board.service.FreeBoardService;
+import com.project.board.vo.FreeBoard;
 import com.project.common.type.StateList;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/board")
 public class FreeBoardContoller {
 
+    private final FreeBoardService freeBoardService;
     @Autowired
     private FreeBoardService boardService;
 
     @RequestMapping("/freeList")
-    public String selectFreeList() {
+    public String selectFreeList(ModelAndView modelAndView) {
+//        ArrayList<FreeBoard> freeBoardList = freeBoardService.sele;
         return "board/freeBoardList";
     }
 }
