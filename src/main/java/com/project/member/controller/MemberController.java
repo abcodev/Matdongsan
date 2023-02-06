fff646d97d9895312372c433debfcded0608279e
JinHoYY <yangkang9879@naver.com>
2023-02-06 15:38:39 +0900

    feat : 마이페이지 페이징, 관심목록

--- src/main/java/com/project/member/controller/MemberController.java
+++ src/main/java/com/project/member/controller/MemberController.java
@@ -2,65 +2,60 @@ package com.project.member.controller;
 
 import com.project.board.dto.FreeBoardResponse;
 import com.project.board.dto.QnaBoardResponse;
-import com.project.board.service.FreeBoardService;
-import com.project.board.vo.FreeBoard;
-import com.project.board.vo.QnaBoard;
+import com.project.common.template.PageInfo;
+import com.project.common.template.Pagination;
 import com.project.common.type.StateList;
-import com.project.member.dto.MemberResponseDto;
+import com.project.member.dto.*;
 import com.project.member.service.MemberService;
 import com.project.member.vo.Member;
+import com.project.realestate.dto.RealEstateDetailDto;
+import com.project.realestate.dto.RealEstateInterestRequest;
 import com.project.realestate.vo.Interest;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
-@RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
-    @RequestMapping(value = "/myPage")
-    public String myPage(HttpSession session, Model model){
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
+    @RequestMapping("/myPage")
+    public ModelAndView ListPaging(@RequestParam(value = "cpage", defaultValue = "1") int currentPage,
+//                                   @RequestParam("estateNo") String estateNo,
+                                   ModelAndView modelAndView, HttpSession session){
         Member m = (Member) session.getAttribute("loginUser");
+        MyPageListRequest req = new MyPageListRequest(currentPage);
+        MyPageListResponse resp = memberService.selectList(req, m);
 
-        ArrayList<FreeBoardResponse> freeBoardList = memberService.getFreeBoardList(m);
-        if(freeBoardList != null){
-            model.addAttribute("freeBoardList", freeBoardList);
-        }
+//        RealEstateDetailDto realEstateDetailDto = memberService.realEstateDetail(estateNo);
+//        modelAndView.addObject("realEstateDetail", realEstateDetailDto);
 
-        ArrayList<QnaBoardResponse> qnaBoardList = memberService.getQnaBoardList(m);
-        if(qnaBoardList != null){
-            model.addAttribute("qnaBoardList", qnaBoardList);
-        }
-
-        ArrayList<Interest> interestList = memberService.getInterestList(m);
-        if(interestList != null){
-            model.addAttribute("interestList", interestList);
-        }
-
-        System.out.println(freeBoardList);
-        System.out.println(qnaBoardList);
-        System.out.println(interestList);
+        modelAndView.addObject("selectAllBoardList", resp.getAllBoardList());
+        modelAndView.addObject("interestList", memberService.getInterestList(m));
+        modelAndView.addObject("pi", resp.getPageInfoCombine());
+        modelAndView.setViewName("member/myPage");
 
 
-        return "member/myPage";
+        return modelAndView;
     }
 
     @RequestMapping(value = "/memberModify")
     public String memberModify(Model model){
+
         model.addAttribute("stateList", StateList.values());
+
         return "member/memberModify";
     }
 
@@ -82,8 +77,37 @@ public class MemberController {
         }
     }
 
+//    @GetMapping("/myPage/interest")
+//    @ResponseBody
+//    public ResponseEntity<Boolean> checkInterest(@RequestParam String estateNo, HttpSession session){
+//        Member loginUser = (Member) session.getAttribute("loginUser");
+//        if (loginUser == null) {
+//            throw new RuntimeException("로그인 하고 오세용");
+//        }
+//        boolean isInterest = MemberService.checkInterest(estateNo, loginUser);
+//        return ResponseEntity.ok(isInterest);
+//    }
+
+    @PostMapping("/myPage")
+    @ResponseBody
+    public ResponseEntity<Void> saveInterest(@RequestBody RealEstateInterestRequest req, HttpSession session){
+        Member loginUser = (Member) session.getAttribute("loginUser");
+        if(loginUser == null){
+            throw  new RuntimeException("로그인 해라 ");
+        }
+        memberService.saveInterest(req, loginUser);
+        return ResponseEntity.ok().build();
+    }
+
+
+
+
+
+
+
+
     /**
-     * 정보수정 - 휴대폰 인증
+     * 휴대폰 인증
      */
     @RequestMapping(value = "/phoneCheck", method = RequestMethod.GET)
     @ResponseBody
@@ -93,12 +117,4 @@ public class MemberController {
         return Integer.toString(randomNumber);
     }
 
-    /**
-     * 회원 탈퇴
-     */
-    @DeleteMapping(value = "/member/delete")
-    public String deleteMember(){
-
-        return null;
-    }
 }
--- src/main/webapp/WEB-INF/views/member/myPage.jsp
+++ src/main/webapp/WEB-INF/views/member/myPage.jsp
@@ -10,11 +10,25 @@
   <script src="https://kit.fontawesome.com/2e05403237.js" crossorigin="anonymous"></script>
   <title>마이페이지</title>
   <link rel="stylesheet" href="<c:url value="/resources/css/member/myPage.css"/>">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <jsp:include page="../template/font.jsp"/>
 </head>
 <body>
 <%@ include file ="../template/header.jsp" %>
-
+<%--<script>--%>
+<%--  window.onload = () => {--%>
+<%--    $.ajax({--%>
+<%--      url: '${pageContext.request.contextPath}/myPage',--%>
+<%--      method: 'GET',--%>
+<%--      data: {--%>
+<%--        'estateNo': ${realEstateDetail.estateNo}--%>
+<%--      },--%>
+<%--      success(data) {--%>
+<%--        $('#checkbox_heart').prop("checked", data);--%>
+<%--      }--%>
+<%--    });--%>
+<%--  }--%>
+<%--</script>--%>
 <div id="content">
   <div id="info_box">
     <div id="userimg">
@@ -45,21 +59,19 @@
       <a href="${pageContext.request.contextPath}/memberModify">정보수정</a>
     </div>
   </div>
+
   <div id="like">
     <h4>내가 찜한 목록</h4>
     <div id="likeList">
-      <table>
+
       <c:forEach items="${interestList}" var="interestList">
-        <tr onclick="location.href='realEstate/detail?estateNo=${interestList.estateNo}'">
-          <td>${interestList.estateNo}</td>
-        </tr>
+          <div id="heart" class="likeInfo">
+            <input id="checkbox_heart" type="checkbox" onchange="changeHeart()">하트
+            <div onclick="location.href='realEstate/detail?estateNo=${interestList.estateNo}'">
+              ${interestList.estateNo}<br>${interestList.bldgNm}<br>${interestList.address}
+            </div>
+          </div>
       </c:forEach>
-      </table>
-      <div class="likeInfo"></div>
-      <div class="likeInfo"></div>
-      <div class="likeInfo"></div>
-      <div class="likeInfo"></div>
-      <div class="likeInfo"></div>
     </div>
   </div>
   <div id="myBoard">
@@ -71,29 +83,66 @@
           <th>게시글 제목</th>
           <th>게시일</th>
         </tr>
-        <c:forEach items="${freeBoardList}" var="freeBoardList">
-          <tr onclick="location.href='board/freeList/detail/${freeBoardList.boardNo}'">
-            <td>${freeBoardList.boardNo}</td>
-            <td>${freeBoardList.boardTitle}</td>
-            <td>${freeBoardList.boardDate}</td>
-          </tr>
-        </c:forEach>
-      </table>
-
-      <table>
-        <c:forEach items="${qnaBoardList}" var="qnaBoardList">
-          <tr onclick="location.href='board/detail/${qnaBoardList.qnaBno}'">
-            <td>${qnaBoardList.qnaBno}</td>
-            <td>${qnaBoardList.qnaTitle}</td>
-            <td>${qnaBoardList.qnaDate}</td>
+        <c:forEach items="${selectAllBoardList}" var="selectAllBoardList">
+          <tr class="myBoard_info" onclick="location.href='board/freeList/detail/${selectAllBoardList.boardNo}'">
+            <td>${selectAllBoardList.boardNo}</td>
+            <td>${selectAllBoardList.boardTitle}</td>
+            <td>${selectAllBoardList.boardDate}</td>
           </tr>
         </c:forEach>
       </table>
     </div>
   </div>
-  <div id="paging">
 
+  <div id="paging">
+    <ul class="pagination">
+        <c:choose>
+            <c:when test="${pi.currentPage eq 1}">
+                <li class="page-item disabled">Previous</li>
+            </c:when>
+            <c:otherwise>
+              <li class="page-item" onclick="retrieveAllBoards(${pi.currentPage - 1})">Previous</li>
+            </c:otherwise>
+        </c:choose>
+
+      <c:forEach var="item" begin="${pi.startPage }" end="${pi.endPage }">
+        <li class="page-item" onclick="retrieveAllBoards(${item})">${item }</li>
+      </c:forEach>
+
+        <c:choose>
+            <c:when test="${pi.currentPage eq pi.maxPage}">
+                <li class="page-item disabled"><a class="page-link" href="#">Next</a></li>
+            </c:when>
+            <c:otherwise>
+                <li class="page-item" onclick="retrieveAllBoards(${pi.currentPage + 1})">Next</li>
+            </c:otherwise>
+        </c:choose>
+
+    </ul>
   </div>
 </div>
+  <script>
+    function retrieveAllBoards(current_page){
+      $.ajax({
+        url: '${pageContext.request.contextPath}/myPage',
+        method: 'GET',
+        data: {
+          cpage: current_page
+        },
+        success(data){
+          $('#myBoardList').empty();
+          console.log($(data).find("#myBoardList"));
+          console.log($(data).find(".myBoard_info").length);
+          if($(data).find(".myBoard_info").length >0) {
+            $('#myBoardList').html($(data).find("#myBoardList"))
+          }else{
+            $('#myBoardList').html('<p>조회된 게시글이 없습니다.</p>');
+          }
+        }
+      })
+    }
+
+  </script>
+
 </body>
 </html>
